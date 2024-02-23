e4b6eb13bb12044e67a3778d1c300e882ada0507
Zoltan Lajko <24388330+zolton1@users.noreply.github.com>
2024-02-23 10:15:53 +0100

    feat: new var - performance_test_execution_timeout_seconds

--- airflow/dags/performance-test/performance-test.py
+++ airflow/dags/performance-test/performance-test.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from datetime import datetime
 from airflow.decorators import task
 from airflow.models import Variable
+from datetime import timedelta
 
 default_args = {
     "owner": "airflow",
@@ -10,17 +11,19 @@ default_args = {
 }
 
 with DAG(
-    "performance_test_dag",
+    "performance-test",
     default_args=default_args,
-    description="performance_test_dag",
+    description="performance-test",
     schedule="*/5 * * * *",
     catchup=False,
     max_active_runs=1,
 ) as dag:
     
-    number_of_tasks = int(Variable.get("performance_test_number_of_tasks", default_var=5))
+    var_number_of_tasks = int(Variable.get("performance_test_number_of_tasks", default_var=5))
 
-    task_list = [f"task_{x}" for x in list(range(0, number_of_tasks))]
+    var_execution_timeout = timedelta(seconds=int(Variable.get("performance_test_execution_timeout_seconds", default_var=10)))
+
+    task_list = [f"task_{x}" for x in list(range(0, var_number_of_tasks))]
 
     a = int(len(task_list) * 0.2)
 
@@ -42,11 +45,11 @@ with DAG(
         import string
         import random
 
-        str_length = int(Variable.get("performance_test_str_length", default_var=100000))
+        var_str_length = int(Variable.get("performance_test_str_length", default_var=1000000))
 
         m = hashlib.sha256()
         txt = "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=str_length)
+            random.choices(string.ascii_uppercase + string.digits, k=var_str_length)
         )
         m.update(txt.encode("utf-8"))
         print(m.hexdigest())
@@ -56,7 +59,7 @@ with DAG(
             for y in b_list:
                 dag.get_task(x) >> dag.get_task(y)
 
-    [pt.override(task_id=ti, retries=0)() for ti in task_list]
+    [pt.override(task_id=ti, retries=0, execution_timeout=var_execution_timeout)() for ti in task_list]
 
     _connect(a_list=task_list_a, b_list=task_list_b)
 
